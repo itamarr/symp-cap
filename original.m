d9faa958ec6e2d963ee2068c963c5b46b2f32ece
gdbgauda <gdbgauda@gmail.com>
2015-05-23 15:25:46 +0200

    It works!!!:-)
    Additional change: a random initial path is generated.

--- original.m
+++ original.m
@@ -2,15 +2,16 @@
 
 %parameters
 tic
-n=1; %(1/2)-times dimension of the space
-m=16; %Number of subdivisions of the [0:1]-interval
-np=2*n;
+n=10; %(1/2)-times dimension of the space
+m=500; %Number of subdivisions of the [0:1]-interval
 eps = 1e-8; %tolerance/ exactness
 
-%initial path x0 in M_m (see paper sec. 2.2, "starting point")
-e1=[1;0];
-e2=[0;1];
-x0=[-e2;-e2;-e2;-e2;-e1;-e1;-e1;-e1;e2;e2;e2;e2;e1;e1;e1;e1]/sqrt(0.125); %Divided by 
+% %initial path x0 in M_m (see paper sec. 2.2, "starting point")
+% %in this case it is a square path
+% e1=[1;0];
+% e2=[0;1];
+% x0=[-e2;-e2;-e2;-e2;-e1;-e1;-e1;-e1;e2;e2;e2;e2;e1;e1;e1;e1]/sqrt(0.125); 
+
 
 %Following lines compute the matrix "A_2n" (see paper sec. 2.1 equation (2.5))
 %Matrix should be removed, and calculations should be done directly
@@ -19,10 +20,40 @@ mJ2n = [zeros(n),-eye(n);eye(n),zeros(n)];
 A2n = zeros(2*m*n);
 for i=1:m
     for j=(i+1):m
-        A2n(np*(i-1)+1:np*i,np*(j-1)+1:np*j)=mJ2n;
+        A2n(2*n*(i-1)+1:2*n*i,2*n*(j-1)+1:2*n*j)=mJ2n;
     end
 end
 %disp(A2n);
+
+%Generate a random initial path which fulfils the constraints
+l=0;
+while (abs(l)<eps) %look at last line in this 'while'-loop...
+    x0=2*(0.5-rand(2*m*n,1)); %random numbers between -1 and 1
+    s=zeros(2*n,1);
+    for i=1:2*n
+        s(i)=sum(x0(i:2*n:2*m*n)); %summing the i-th coordiantes, i=1,..,2n
+        for j=0:m-1
+            x0(j*2*n+i)=x0(j*2*n+i)-s(i)/m; %now, sum of vectors is 0
+        end
+    end
+    l =x0'*A2n*x0; %this is needed for the last constraint
+end
+
+if(l<0) %in this case, the path has to be inverted
+    x0p=x0;
+    for i=0:m-1
+        j=m-1-i;
+        x0(2*n*i+1:2*n*(i+1))=x0p(2*n*j+1:2*n*(j+1));
+    end
+end
+
+%now that l is positive, rescale x such that the last constraint is
+%fulfilled.
+l =x0'*A2n*x0;
+x0=x0*m/sqrt(l);
+%disp(x0'*A2n*x0/m^2 - 1);
+
+%Initial value computed. Start the minimization process!!!
 
 k=0; %Counts the iteration 
 carryOn = true;
@@ -77,5 +108,6 @@ disp(x);
 disp('Iterations:');
 disp(k);
 disp('Minimal action:');
-disp(2*F(x,m,n));
+action=2*F(x,m,n);
+disp(action);
 toc
