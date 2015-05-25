81aefb81076e8caf59430a3652c22b3345901ef5
gdbgauda <gdbgauda@gmail.com>
2015-05-25 16:04:20 +0200

    Update G.m

--- G.m
+++ G.m
@@ -1,7 +1,16 @@
 function [ y ] = G(x,n)
 %G Summary of this function goes here
 %   
+a=[1;2;3;4;5;6;7;8;9;10];
+if(size(a,1)~=n)
+    error('function G: Dimension mismatch!');
+end
 
-y=dot(x,x)/4;
+sum=0;
+for i=1:n
+    sum=sum+(a(i)^2)*(x(i)^2+x(i+n)^2);
+end
+
+y=sum/4;
 
 end
