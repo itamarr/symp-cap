c770bf155ea57116f5864d940ea5b4db5af78674
Itamar Racuh <itamar.rauch@gmail.com>
2015-05-26 11:33:39 +0300

    Removed PointsToHalfSpaces in NormOfPolarBody

--- NormOfPolarBody.m
+++ NormOfPolarBody.m
@@ -2,7 +2,7 @@ function [ n ] = NormOfPolarBody( P, y )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-    [A, b] = PointsToHalfSpaces(P);
+    %[A, b] = PointsToHalfSpaces(P);
     %%A(~any(A,2),:) = [];
     %%b(~any(b,2),:) = [];
     
