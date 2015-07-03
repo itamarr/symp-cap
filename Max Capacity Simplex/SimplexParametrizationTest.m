3d563421ae85f2a8357c05d18c551f8744eb50c3
Itamar Racuh <itamar.rauch@gmail.com>
2015-07-03 18:52:10 +0300

    Fixed symbols in script

--- Max Capacity Simplex/SimplexParametrizationTest.m
+++ Max Capacity Simplex/SimplexParametrizationTest.m
@@ -1,9 +1,9 @@
 A = GenerateSimplexParamertization(2);
 
 % plug in the numbers for regular polygon in R^4
-reg = subs(A, 't1_2',1);
-reg = subs(reg, 't1_3',0);
-reg = subs(reg, 't1_4',0);
-reg = subs(reg, 't2_3',0);
-reg = subs(reg, 't2_4',0);
-reg = subs(reg, 't3_4',1);
+reg = subs(A, 'a1_2',1);
+reg = subs(reg, 'a1_3',0);
+reg = subs(reg, 'a1_4',0);
+reg = subs(reg, 'a2_3',0);
+reg = subs(reg, 'a2_4',0);
+reg = subs(reg, 'a3_4',1);
