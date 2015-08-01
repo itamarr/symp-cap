b8d19befc33e637460441089d9bcbe0e9f255e97
gdbgauda <gdbgauda@gmail.com>
2015-08-01 08:16:20 -0400

    first attempts at a visualization

--- Projection-onto-simplex/prSimplex.m
+++ Projection-onto-simplex/prSimplex.m
@@ -1,4 +1,4 @@
-function [faceArray,alpha,projectionData] = prSimplex(n,simplex,path,tolerance)
+function [faceArray,alpha,projectionData,distMatrix] = prSimplex(n,simplex,path,tolerance)
 %PRSIMPLEX Projects path on simplex boundary onto the separate faces
 %   INPUT:
 %
@@ -32,9 +32,12 @@ function [faceArray,alpha,projectionData] = prSimplex(n,simplex,path,tolerance)
 %       v_new=(B^-1)*(v_old-v_tr)
 %   where the last coordinate of v_new is zero and is omitted, so v_new is
 %   (2n-1)-dimensional.
+%
+%   distMatrix = distances of the path vectors to the faces, distance of
+%   vector j from face i is stored in distMatrix(i,j)
 
 %%  Some preparations: Cheching that the input is sensible
-if rem(n,1)~=0
+if rem(n,1)~=0 || n<=0
     error('Invalid value of "n"');
 end
 dim=2*n;
@@ -77,13 +80,15 @@ if abs(det(S))<tolerance
     error('Simplex is (almost) degenerate!');
 end
 
+numOfFaces=size(simplex,2);
+
 %create empty data cell array
-projectionData = cell(size(simplex,2),2);
+projectionData = cell(numOfFaces,2);
 
 %Determine the base change information for every face
 
 %store normal vectors for every face separately
-numOfFaces=size(simplex,2);
+
 normalVecs=zeros(dim,numOfFaces);
 
 for j=1:numOfFaces
@@ -94,10 +99,10 @@ for j=1:numOfFaces
     else
         k=1;
     end
-    projectionData{j,1}=simplex(:,k); %store v_tr
     S=simplex;
     S(:,j)=[];
     barycenterS=sum(S,2)/size(S,2);
+    projectionData{j,1}=S(:,k); %store v_tr
     S=simplexSetCornerToZero(S,k);
     %determine vector orthogonal to this
     N=null(S');
@@ -116,13 +121,14 @@ for j=1:numOfFaces
     %From the above computed matrix S and vector n, determine the base
     %change matrix 'pMat'
     pMat=[gramSchmidt(S),N];
-    if norm(pMat'*pMat-eye(dim))>tolerance
+    shouldBeZero=pMat'*pMat-eye(dim);
+    if norm(shouldBeZero(:))>tolerance
         error('Base transform matrix is not orthogonal... Check your code, man!');
     else
         projectionData{j,2}=pMat;
     end
 end
-%%  Assign the points to a face
+%%  Assign the points to a face using the foliation
 %Calculate foliation vectors
 J = [zeros(n),eye(n);-eye(n),zeros(n)];
 foliationVecs=J*normalVecs;
@@ -138,7 +144,7 @@ for i=1:(m-1)
 end
 y(:,m)=pathVecs(:,1)-pathVecs(:,m);
 
-% dotArray stores the inner products of a connecting vector with each
+% dotMatrix stores the inner products of a connecting vector with each
 % foliation vector as a column.
 
 % here size(simplex,2) means the number of vertices in P. In the case of a
@@ -146,15 +152,15 @@ y(:,m)=pathVecs(:,1)-pathVecs(:,m);
 % is not true. We will need additional input to the functionto account for 
 % this in the future.
 
-dotArray=zeros(size(simplex,2),m);
+dotMatrix=zeros(size(simplex,2),m);
 for i=1:m
     for j=1:(dim+1)
-        dotArray(j,i)=abs(dot(y(:,i),foliationVecs(:,j)));
+        dotMatrix(j,i)=abs(dot(y(:,i),foliationVecs(:,j)));
     end
 end
 
 %
-[M,faceArray]=max(dotArray);
+[M,faceArray]=max(dotMatrix);
 
 alpha=zeros(1,m);
 for i=1:m
@@ -163,5 +169,69 @@ for i=1:m
     alpha(i)=dot(yy,ff)/norm(yy);
 end
 alpha=acos(alpha);
+
+
+%%  Calculate distances of path vectors from faces, and if n<=2, plot
+
+distMatrix=zeros(numOfFaces,m);
+
+if n==2
+    %first column will contain path vectors, second column will contain
+    %indices from the set {1,...,m} corresponding to path vectors
+    projectedVectors=cell(numOfFaces,2);
+end
+
+for k=1:numOfFaces
+    B=projectionData{k,2};
+    vTr=projectionData{k,1};
+    for i=1:m
+        ProjectedVec=B\(pathVecs(:,i)-vTr); %B\v calls a more efficient
+        %Matlab algorithm to compute (B^-1)*v
+        dist=abs(ProjectedVec(dim));
+        %if n==2 && dist<tolerance
+        %Above line commented out because this doesn't work!
+        if n==2 && faceArray(i)==k
+            %Information for drawing and labeling
+            projectedVectors{k,1}=[projectedVectors{k,1},ProjectedVec(1:(dim-1))];
+            projectedVectors{k,2}=[projectedVectors{k,2},i];
+        end
+        distMatrix(k,i)=dist;
+    end
+end
+
+if n==1
+    drawSimplex=[simplex,simplex(:,1)];
+    figure
+    title('trajectory on simplex')
+    plot(drawSimplex(1,:),drawSimplex(2,:))
+    hold on
+    plot(pathVecs(1,:),pathVecs(2,:))
+    hold on
+    labels=cellstr(num2str([1:m]'))';
+    text(pathVecs(1,:),pathVecs(2,:),labels)
+elseif n==2
+    for j=1:numOfFaces
+        S=simplex;
+        S(:,j)=[];
+        B=projectionData{j,2};
+        vTr=projectionData{j,1};
+        S=B\(S-repmat(vTr,1,size(S,2)));
+        if norm(S(dim,:))>tolerance
+            error('Projection of simplex corners not working as expected..');
+        end
+        S=S(1:(dim-1),:);
+        drawSimplexFace=[S,S(:,[1,3,2,4])];
+        ttl=strcat('face ',num2str(j));
+        figure('name',ttl)
+        title(ttl)
+        plot3(drawSimplexFace(1,:),drawSimplexFace(2,:),drawSimplexFace(3,:))
+        hold on
+        PP=projectedVectors{j,1};
+        plot3(PP(1,:),PP(2,:),PP(3,:))
+        hold on
+        labels=cellstr(num2str(projectedVectors{j,2}'))';
+        text(PP(1,:),PP(2,:),PP(3,:),labels)
+    end
+end
 
 end
