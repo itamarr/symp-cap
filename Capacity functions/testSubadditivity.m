a24386aef72971b4a9d1e3feaa193faabf1e5c45
gdbgauda <gdbgauda@gmail.com>
2015-08-05 15:53:18 -0400

    Update testSubadditivity.m

--- Capacity functions/testSubadditivity.m
+++ Capacity functions/testSubadditivity.m
@@ -1,14 +1,33 @@
-tic
-% standardSimplex=subdividebyhyperplane(0,0,1,1,0,0);
-% maxSymmSimplex=subdividebyhyperplane(1,1,1,1,1,1);
 %time in h:m:s
 h=0;
 m=10;
 s=0;
 t=h*3600+m*60+s;
-randSimplices=cell(0);
-numOfTests=30;
-for i=1:numOfTests
+randSimplices=cell(0)
+numOfRandSimplices=30;
+% disp('testing candidate 1');
+% i=0;
+% while i<numOfTriesForCandidates
+%     try
+%         randSimplices{end+1}=subdividebyhyperplane(3.320312570057692,5.302492307431258,4.238524246629064,0.153229743527471,3.520270034036538,3.530366695263731);
+%         i=i+1;
+%     catch E
+%         warning('horrible stuff happened');
+%     end
+% end
+% disp('testing candidate 2');
+% i=0;
+% while i<numOfTriesForCandidates
+%     try
+%         randSimplices{end+1}=subdividebyhyperplane(6.503699839036440,5.312926257687318,2.629042294349858,5.058798957733863,2.314754857765802,3.679614782989666);
+%         i=i+1;
+%     catch E
+%         warning('horrible stuff happened');
+%     end 
+% end
+disp('testing random simplices');
+i=0;
+while i<numOfRandSimplices
     A=10*rand;
     B=10*rand;
     C=10*rand;
@@ -17,8 +36,9 @@ for i=1:numOfTests
     F=10*rand;
     try
         randSimplices{end+1}=subdividebyhyperplane(A,B,C,D,E,F);
+        i=i+1;
     catch E
-        warning('horrible stuff happened');
-        i=i-1;
+        %warning('horrible stuff happened');
     end
 end
+save data.mat randSimplices
